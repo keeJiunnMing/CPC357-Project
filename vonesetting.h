257b8abb4310dc9b2e73dae555234f81469b3dbf
LSKeat <lskeat@student.usm.my>
2025-01-12 13:23:50 +0800

    Add files via upload

---
+++ vonesetting.h
@@ -0,0 +1,17 @@
+#if !defined (VONESETTING_H)
+#define VONESETTING_H
+#else
+#error Multiple includes of vonesetting.h
+#endif
+
+#define WIFI_SSID "cslab"
+#define WIFI_PASSWORD "aksesg31"
+
+#define MQTT_SERVER "mqtt.v-one.my"
+#define MQTT_PORT 8883
+#define MQTT_USERNAME "r7xM6ccrZmGKADRi"  //Replace this with the Access Token of YOUR gateway
+#define MQTT_PASSWORD " "
+
+#define GATEWAYID "3accc8a8-8355-4880-8dea-67b4263a51e3"  //Replace this with the gatewayID of your gateway
+#define INTERVAL 1000 //1S
+#define INTERVAL2 500 //0.5S
